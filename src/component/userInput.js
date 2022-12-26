eb3848f80e7777072e7bae9e2945af98c00aa084
Amol shelke <shelkeamol897@gmail.com>
2022-12-26 23:49:23 +0530

    Generated Secret Number using Math.random Function

--- src/component/userInput.js
+++ src/component/userInput.js
@@ -2,10 +2,9 @@ import React, { useState } from "react";
 
 const UserInput = () => {
   const [userGuessNumber, setUserGuessNumber] = useState("");
-  const [secretNumber, setSecretNumber] = useState("");
 
-  //   const secretNumber = Math.ceil(Math.random() * 100) + 1;
-  //   console.log(secretNumber);
+  const secretNumber = Math.ceil(Math.random() * 100) + 1;
+  console.log(secretNumber);
 
   const onChangeHanlder = (e) => {
     setUserGuessNumber(+e.target.value);
